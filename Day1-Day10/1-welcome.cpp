560353619d142010fa09e0ecca5179e671274991
Vaishnavi7279 <srivastava93vaishnavi@gmail.com>
2024-07-05 23:55:48 +0530

    Day 1 Updates: 1-D Array

---
+++ Day1-Day10/1-welcome.cpp
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+//hello world
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    cout<<"Welcome to GFG 100 Days-Of-Code!";
+    return 0;
+=======
+//hello world
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    cout<<"Welcome to GFG 100 Days-Of-Code!";
+    return 0;
+>>>>>>> d0b563efb883899e01290b3d059a582c0171e02b
+}
